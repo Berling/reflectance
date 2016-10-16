ddd54af13e0650fd79d65a20748d85049ddefc2a
Georg Schäfer <georg.peter.schaefer@gmail.com>
2016-10-16 13:51:02 +0200

    Add abstract_property.

---
+++ include/reflectance/abstract_property.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <string>
+
+namespace reflectance {
+class abstract_property {
+private:
+	std::string _name;
+
+public:
+	abstract_property(const std::string& name) : _name{name} {}
+	virtual ~abstract_property() = 0;
+
+	auto& name() const noexcept {
+		return _name;
+	}
+
+	virtual size_t hash_code() const noexcept = 0;
+};
+
+abstract_property::~abstract_property() {}
+}
