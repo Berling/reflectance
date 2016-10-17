4077173e6259ddb7407d78391405903ab28aa964
Georg Schäfer <georg.peter.schaefer@gmail.com>
2016-10-17 11:30:14 +0200

    Add bad property cast exception.

--- include/reflectance/exception.hpp
+++ include/reflectance/exception.hpp
@@ -8,4 +8,20 @@ public:
 	bad_accessor_call(const std::string& what_arg) : std::runtime_error{what_arg} {}
 	virtual ~bad_accessor_call() = default;
 };
+
+class bad_property_cast : public std::bad_cast {
+private:
+	std::string _what_arg;
+
+public:
+	bad_property_cast(const std::string& property_name) {
+		_what_arg = "attempted cast to wrong type with property " + property_name;
+	}
+
+	virtual ~bad_property_cast() = default;
+
+	virtual const char* what() const noexcept {
+		return _what_arg.c_str();
+	}
+};
 }
