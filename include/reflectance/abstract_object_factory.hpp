70da7fea631f36ac466bcdcd6736fe0d0ff07b3a
Georg Schäfer <georg.peter.schaefer@gmail.com>
2016-10-28 10:54:49 +0200

    Add named argument list to object factory.

--- include/reflectance/abstract_object_factory.hpp
+++ include/reflectance/abstract_object_factory.hpp
@@ -1,15 +1,19 @@
 #pragma once
 
+#include <string>
+#include <unordered_map>
 #include <vector>
 
 #include <reflectance/any.hpp>
 
 namespace reflectance {
 using argument_list = std::vector<any>;
+using named_argument_list = std::unordered_map<std::string, any>;
 
 class abstract_object_factory {
 public:
 	virtual ~abstract_object_factory() = default;
 	virtual any construct(const argument_list& argument_list) = 0;
+	virtual any construct(const named_argument_list& argument_list) = 0;
 };
 }
--- include/reflectance/object_factory.hpp
+++ include/reflectance/object_factory.hpp
@@ -10,15 +10,26 @@ class object_factory : public abstract_object_factory {
 public:
 	virtual any construct(const argument_list& argument_list) {
 		auto& properties = T::properties();
-
 		if(argument_list.size() != properties.size()) {
 			throw std::invalid_argument{"invalid length of argument list"};
 		}
-
 		auto instance = reflectance::make_any<T>();
 		for(auto index = argument_list::size_type{0}; index < argument_list.size(); ++index) {
 			auto& property = properties.at(index);
 			auto& argument = argument_list[index];
+			auto& assigner = properties.assigner(property.type().hash_code());
+			assigner.assign(property, instance, argument);
+		}
+		return instance;
+	}
+
+	virtual any construct(const named_argument_list& argument_list) {
+		auto& properties = T::properties();
+		auto instance = reflectance::make_any<T>();
+		for(auto& argument_pair : argument_list) {
+			auto& property_name = argument_pair.first;
+			auto& argument = argument_pair.second;
+			auto& property = properties.at(property_name);
 			auto& assigner = properties.assigner(property.type().hash_code());
 			assigner.assign(property, instance, argument);
 		}
